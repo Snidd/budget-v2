f4a847ebf3caf1bfd651dad6c41c32b8ec91930a
Magnus Kjellberg <magnus.kjellberg@istone.com>
2022-08-22 16:47:25 +0200

    Added Previous months to Summary.

--- src/routes/months/[year]/[month]/MonthSummary.svelte
+++ src/routes/months/[year]/[month]/MonthSummary.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import CategoryBadge from '$components/badges/CategoryBadge.svelte';
 	import type { InferQueryOutput } from '$lib/client/trpc';
-	import { formatSEK } from '$lib/utils';
+	import { formatMonth, formatSEK } from '$lib/utils';
 	import Decimal from 'decimal.js';
-
+	import { months } from '$lib/stores/months';
 	type Expenses = InferQueryOutput<'expenses:listByMonth'>;
 
 	export let expenses: Expenses;
@@ -28,46 +28,61 @@
 		return categories;
 	};
 
-	const getCategorySum = (
+	const getCategoriesSum = (
 		expensesList: Expenses,
 		monthId: number,
 		categories: { name: string; color: string; isIncome: boolean }[]
 	) => {
-		const categoriesWithValues = categories.map((cat) => {
-			let values = expensesList
-				.filter((exp) => exp.category.name === cat.name)
-				.map((exp) => exp.expenseValue)
-				.flat()
-				.filter((ev) => ev.monthId === monthId);
-			return {
-				...cat,
-				ev: values
-			};
-		});
+		return categories.map((cat) => getCategorySum(expensesList, monthId, cat));
+	};
 
-		const categoriesWithSum = categoriesWithValues.map((catv) => {
-			const sum = catv.ev.reduce((prev, cur) => {
-				if (cur.value !== null) {
-					return prev.plus(new Decimal(cur.value.toString()));
-				}
-				return prev;
-			}, new Decimal(0));
-			return { ...catv, sum: sum };
-		});
+	const getCategorySum = (
+		expensesList: Expenses,
+		monthId: number,
+		category: { name: string; color: string; isIncome: boolean }
+	) => {
+		let values = expensesList
+			.filter((exp) => exp.category.name === category.name)
+			.map((exp) => exp.expenseValue)
+			.flat()
+			.filter((ev) => ev.monthId === monthId);
+		let categoryWithValues = { ...category, ev: values };
 
-		return categoriesWithSum;
+		const sum = categoryWithValues.ev.reduce((prev, cur) => {
+			if (cur.value !== null) {
+				return prev.plus(new Decimal(cur.value.toString()));
+			}
+			return prev;
+		}, new Decimal(0));
+
+		let categoryWithSum = { ...categoryWithValues, sum: sum };
+		return categoryWithSum;
+	};
+
+	const getTotalFromSums = (
+		catWithSum: Partial<{ sum: Decimal; isIncome: boolean }[]>
+	): Decimal => {
+		if (catWithSum === undefined || catWithSum.length === 0) return new Decimal(0);
+
+		return catWithSum.reduce((prev, cur) => {
+			if (cur === undefined) return prev;
+			if (cur.isIncome) {
+				return prev.plus(cur.sum);
+			} else {
+				return prev.minus(cur.sum);
+			}
+		}, new Decimal(0));
 	};
 
 	$: categories = getCategories(expenses);
-	$: categoriesWithSum = getCategorySum(expenses, monthId, categories);
+	$: categoriesWithSum = getCategoriesSum(expenses, monthId, categories);
 
-	$: total = categoriesWithSum.reduce((prev, cur) => {
-		if (cur.isIncome) {
-			return prev.plus(cur.sum);
-		} else {
-			return prev.minus(cur.sum);
-		}
-	}, new Decimal(0));
+	$: total = getTotalFromSums(categoriesWithSum);
+
+	$: lastFiveMonths = $months.slice(
+		$months.length - 5 > 0 ? $months.length : 0,
+		$months.length - 1
+	);
 </script>
 
 <div class="flex flex-col w-full border-opacity-50">
@@ -76,6 +91,9 @@
 		<thead>
 			<tr>
 				<th>Kategori</th>
+				{#each lastFiveMonths as month}
+					<th>{formatMonth(month.year, month.month)}</th>
+				{/each}
 				<th>Summa</th>
 			</tr>
 		</thead>
@@ -83,11 +101,32 @@
 			{#each categoriesWithSum as category}
 				<tr>
 					<td><CategoryBadge {category} /></td>
+					{#each lastFiveMonths as month, index}
+						{@const catWithSum = getCategorySum(expenses, month.id, category)}
+						{@const lastSum = getCategorySum(expenses, lastFiveMonths[index - 1]?.id, category)}
+						{@const diff = lastSum.sum.equals(0)
+							? new Decimal(0)
+							: lastSum.sum.minus(catWithSum.sum)}
+						{#if month.id !== monthId}
+							<td
+								>{category.isIncome ? '' : '-'}
+								{formatSEK(catWithSum.sum)}
+								{#if diff.greaterThan(0)}<span class="text-green-700">+{formatSEK(diff)}</span
+									>{:else if diff.lessThan(0)}<span class="text-red-600">{formatSEK(diff)}</span
+									>{/if}</td
+							>
+						{/if}
+					{/each}
 					<td>{category.isIncome ? '' : '-'} {formatSEK(category.sum)}</td>
 				</tr>
 			{/each}
 			<tr class="bg-accent">
 				<td class="bg-accent"><span class="font-bold">Totalt</span></td>
+				{#each lastFiveMonths as month}
+					{@const sums = getCategoriesSum(expenses, month.id, categories)}
+					{@const totals = getTotalFromSums(sums)}
+					<td class="bg-accent">{totals > new Decimal(0) ? '+' : ''} {formatSEK(totals)}</td>
+				{/each}
 				<td class="bg-accent">{total > new Decimal(0) ? '+' : ''} {formatSEK(total)}</td>
 			</tr>
 		</tbody>
