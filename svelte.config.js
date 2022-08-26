8d3e10fa0905187613498af0a15e492ad910a9ce
Magnus Kjellberg <magnus.kjellberg@istone.com>
2022-08-26 10:38:12 +0200

    Testing auto adapter instead of vercel adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,20 +10,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: false,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		}),
+		adapter: adapter(),
 		alias: {
 			$components: 'src/components'
 		}
